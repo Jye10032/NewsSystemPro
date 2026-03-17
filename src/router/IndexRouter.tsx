028b6c31bdb1678cf067aff1d172c8803c724381
Jye10032 <736891807@qq.com>
2026-03-17 22:44:20 +0800

    perf: lazy load top-level routes

--- src/router/IndexRouter.tsx
+++ src/router/IndexRouter.tsx
@@ -1,24 +1,29 @@
+import { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Login from '../modules/login/Login'
-import NewsSandBox from '../sandbox/NewsSandBox'
-import Detail from '../modules/visitor/pages/Detail'
-import News from '../modules/visitor/pages/News'
+import { Spin } from 'antd'
 import AuthRoute from './AuthRoute'
+
+const Login = lazy(() => import('../modules/login/Login'))
+const NewsSandBox = lazy(() => import('../sandbox/NewsSandBox'))
+const Detail = lazy(() => import('../modules/visitor/pages/Detail'))
+const News = lazy(() => import('../modules/visitor/pages/News'))
 
 /**
  * 基础路由，在App.js中引入
  */
 export default function IndexRouter() {
     return (
-        <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/news" element={<News />} />
-            <Route path="/detail/:id" element={<Detail />} />
-            <Route path="/*" element={
-                <AuthRoute>
-                    <NewsSandBox />
-                </AuthRoute>
-            } />
-        </Routes>
+        <Suspense fallback={<Spin size="large" tip="页面加载中..." />}>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/news" element={<News />} />
+                <Route path="/detail/:id" element={<Detail />} />
+                <Route path="/*" element={
+                    <AuthRoute>
+                        <NewsSandBox />
+                    </AuthRoute>
+                } />
+            </Routes>
+        </Suspense>
     )
 }
